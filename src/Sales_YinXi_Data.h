a5f7d519abe7e8f47b107640bf25916792e643f1
YinXi <yinximail@gmail.com>
2020-06-10 16:53:12 +0800

    Add @ifdndef

--- src/Sales_YinXi_Data.h
+++ src/Sales_YinXi_Data.h
@@ -1,7 +1,10 @@
-#include <xstring>
+#ifndef SALES_YINXI_DATA_H
+#define SALES_YINXI_DATA_H
+#include <string>
 
 struct Sales_Data_YinXi {
   std::string bookNo;
   unsigned units_sold = 0;
   double revenue = 0.0;
 };
+#endif  // !SALES_YINXI_DATA_H
--- src/struct-test.cpp
+++ src/struct-test.cpp
@@ -2,10 +2,13 @@
 //
 //}
 
+#include <iostream>
+
 #include "Sales_YinXi_Data.h"
 
 int main() {
   Sales_Data_YinXi sales;
+  std::cout << sales.bookNo << sales.revenue << sales.units_sold << std::endl;
 
   return 0;
 }
