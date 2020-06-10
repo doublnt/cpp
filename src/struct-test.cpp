818f6cd28e3899e7a13f0291118478f0fa723c81
YinXi <yinximail@gmail.com>
2020-06-10 21:02:02 +0800

    string test

--- src/struct-test.cpp
+++ src/struct-test.cpp
@@ -10,5 +10,29 @@ int main() {
   Sales_Data_YinXi sales;
   std::cout << sales.bookNo << sales.revenue << sales.units_sold << std::endl;
 
+  // std::string str;
+
+  // std::cin >> str;
+
+  // std::cout << str << std::endl;
+
+  // std::string s1, s2;
+
+  // std::cin >> s1 >> s2;
+
+  // std::cout << s1 << s2 << std::endl;
+
+ /* std::string s3;
+
+  while (std::cin >> s3) {
+    std::cout << s3 << std::endl;
+  }*/
+
+  std::string s4;
+
+  while (std::getline(std::cin, s4)) {
+    std::cout << s4 << s4.size() << std::endl;
+  }
+
   return 0;
 }
