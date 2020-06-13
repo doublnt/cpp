155b4366610df837479613c0e756cb8dd3d461e6
Xi Yin <yinximail@gmail.com>
2020-06-13 09:51:13 +0800

    iterator test

--- src/iterator-test.cpp
+++ src/iterator-test.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include <vector>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
+using std::vector;
+
+void print_vector(vector<unsigned> vector_param) {
+  for (auto item : vector_param) {
+    cout << item;
+  }
+  cout << endl;
+}
+
+void print_vector_length(vector<unsigned>::iterator v1,
+                         vector<unsigned>::iterator v2) {
+  cout << v2 - v1 << endl;
+}
+
+void p99_323() {
+  vector<unsigned> unsigned_vector1(10, 2);
+  auto vector_begin = unsigned_vector1.begin();
+
+  print_vector(unsigned_vector1);
+
+  for (auto vector_begin = unsigned_vector1.begin();
+       vector_begin != unsigned_vector1.end(); vector_begin++) {
+    *vector_begin = *vector_begin * 2;
+  }
+
+  print_vector(unsigned_vector1);
+
+  print_vector_length(unsigned_vector1.begin(), unsigned_vector1.end());
+}
 
 int main() {
   string s("hello, lmy");
@@ -12,4 +42,6 @@ int main() {
   }
 
   cout << s << endl;
+
+  p99_323();
 }
