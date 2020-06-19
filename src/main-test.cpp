b8b4714ace389b8bbc8c9ae1e3852666e2180d0a
Xi Yin <yinximail@gmail.com>
2020-06-19 21:45:56 +0800

    Update return demo

--- src/main-test.cpp
+++ src/main-test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using std::cin;
 using std::cout;
@@ -14,7 +15,20 @@ void error_msg(std::initializer_list<string> li) {
   cout << endl;
 }
 
+int reture_demo() {
+  std::vector<int> vector_int(10, 0);
+  for (auto item = vector_int.begin(); item != vector_int.end(); ++item) {
+    cout << *item << endl;
+
+    return 11;
+  }
+
+  return 22;
+}
+
 int main() {
   error_msg({"Error Message", "Okay Message"});
+
+  cout << reture_demo() << endl;
   return 0;
 }
