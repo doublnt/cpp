6f8d9e7cbd186f8260b5f81a4af316db6aedd0b6
YinXi <yinximail@gmail.com>
2020-10-30 23:38:11 +0800

    Add is land cpp code

---
+++ src/is-land.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int getLandPerimeter(vector<vector<int>>& ve){
+	
+}
+
+int islandPerimeter(vector<vector<int>>& grid){
+	for(auto begin = grid[0])
+}
+
+int main(){
+	return 0;
+}
