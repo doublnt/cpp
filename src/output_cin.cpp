39d06f64d1152aea7b1175032f8f3e7f6b5b0fe4
YinXi <yinximail@gmail.com>
2020-12-09 20:19:32 +0800

    Add cin

---
+++ src/output_cin.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+	std::cout << "Input two:" << std::endl;
+	int c1, c2;
+	std::cin >> c1 >> c2;
+
+	std::cout << "The sum is" << c1+c2 << std::endl;
+	return 0;
+}
